d9e51eb34d1d8c365763d784b7d2111ad8ca91a9
LiuJunjie0619 <jy024426122@sina.com>
2018-06-30 15:24:48 +0800

    iterator20180630
    iterator 2018 06 30

--- 016_for_range/main.cpp
+++ 016_for_range/main.cpp
@@ -1,18 +1,57 @@
 #include <iostream>
-
-using namespace std;
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
 void Show(double x)
 {
     std::cout << x << std::endl;
 }
 
+template<typename T>
+bool lessonThan(T & a,T & b)
+{
+    if(a < b)
+        return true;
+    else
+        return false;
+}
+template<typename T>
+bool moreThan(T & a,T & b)
+{
+    if(a >= b)
+        return true;
+    else
+        return false;
+}
 int main()
 {
     double prices[5] ={4.99,10.99,6.87,7.99,8.49};
     for(double x : prices)
         std::cout << x << std::endl;
 
+    std::cout << "--------------------------------" << std::endl;
+
+    std::sort(prices,prices+5);
+    std::for_each(prices,prices+5,Show);
+
+    std::cout << "--------------------------------" << std::endl;
+    std::random_shuffle(&prices[0],&prices[5]);
+    std::for_each(&prices[0],&prices[5],Show);
+
+    std::cout << "*********************************" << std::endl;
+    int casts[10] = {6,7,2,9,4,11,8,7,10,5};
+    std::vector<int> dice(10);
+    std::copy(casts,casts+10,dice.begin());
+
+    std::sort(dice.begin(),dice.end());
+    std::ostream_iterator<int,char> out_iter(std::cout,"\n");
+    //*out_iter++ = 15;   //works licke std::cout << 15 << " " << std::endl;
+
+    std::copy(dice.begin(),dice.end(),out_iter);
+    std::cout << "*********************************" << std::endl;
+    std::sort(dice.begin(),dice.end(),::moreThan<int>);
+    std::copy(dice.begin(),dice.end(),std::ostream_iterator<int,char>(std::cout,"\n"));
 
     return 0;
 }
